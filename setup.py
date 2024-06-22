f989927effa6fb2561d61b55bd737c8943222f65
Rahul Kr Sharma <Rahusa960@gmail.com>
2024-06-22 17:20:38 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup # type: ignore
+from typing import List
+
+HPEN_E_DOT='-e .'
+
+def get_requirements(filename:str)-> List[str]:
+    requirements=[]
+    with open (filename) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if HPEN_E_DOT in requirements:
+            requirements.remove(HPEN_E_DOT)
+            
+    return requirements
+
+setup(
+name='HEATWAVES FORECAST PROJECT',
+version='0.0.1',
+author='rah',
+author_email='rahulsa960@gmail.com',
+packages=find_packages(),
+install_requirements=get_requirements('requirements.txt')
+
+)
+    
